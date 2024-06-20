ffcaad50a15a257535494c6f839374ca6e427043
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-06-20 16:55:46 +0300

    added catcher notif

--- app/src/main/java/com/example/notifsoundssetter/MainActivity.java
+++ app/src/main/java/com/example/notifsoundssetter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.notifsoundssetter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        startActivity(intent);
+
 
         MainActivity contextMain=this;
         Button buttonOne = (Button) findViewById(R.id.testNotif);
---
+++ app/src/main/java/com/example/notifsoundssetter/modules/NotifCatcher.java
@@ -0,0 +1,65 @@
+package com.example.notifsoundssetter.modules;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotifCatcher extends NotificationListenerService {
+
+
+    Context context=null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        IBinder mIBinder = super.onBind(intent);
+
+        return mIBinder;
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        try {notifMsgCatch(sbn);
+        }catch (Exception f){f.printStackTrace();}
+    }
+
+
+    void notifMsgCatch(StatusBarNotification sbn){
+//        if (context==null)context= MainActivity.contextMainActivity;
+
+        Bundle extras=null;
+        try {extras = sbn.getNotification().extras;}catch (Exception d){return;}
+
+        String pack ="";
+        String title ="";
+        String text ="";
+        PendingIntent intent =null;
+
+        try {
+            Notification n = sbn.getNotification(); intent = n.contentIntent;}catch (Exception e){}
+        try {  pack = sbn.getPackageName();  if (pack==null)pack=""; }catch (Exception e){}
+        try {  title = extras.getString("android.title"); if (title==null)title=""; }catch (Exception e){}
+        try {  text = extras.getCharSequence("android.text").toString();  }catch (Exception e){}
+
+
+        new String();
+
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+//        Log.i("Msg","Notification Removed");
+    }
+
+}
